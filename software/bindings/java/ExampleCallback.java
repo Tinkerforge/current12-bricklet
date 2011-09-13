a356e7781cc7425f20a133b3ef06e2b755db3c0d
Olaf Lüke <olaf@tinkerforge.com>
2011-09-13 18:59:40 +0200

    add java bindings and examples

---
+++ software/bindings/java/ExampleCallback.java
@@ -0,0 +1,34 @@
+import com.tinkerforge.BrickletCurrent12;
+import com.tinkerforge.IPConnection;
+
+public class ExampleCallback {
+	private static final String host = new String("localhost");
+	private static final int port = 4223;
+	private static final String UID = new String("ABC"); // Change to your UID
+	
+	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+	//       might normally want to catch are described in the commnents below
+	public static void main(String args[]) throws Exception {
+		IPConnection ipcon = new IPConnection(host, port); // Create connection to brickd (Can throw IOException)
+
+		BrickletCurrent12 c12 = new BrickletCurrent12(UID); // Create device object
+		ipcon.addDevice(c12); // Add device to ip connection (Can throw IPConnection.TimeoutException)
+		// Don't use device before it is added to a connection
+		
+
+		// Set Period for current callback to 1s (1000ms)
+		// Note: The current callback is only called every second if the 
+		//       current has changed since the last call!
+		c12.setCurrentCallbackPeriod(1000);
+
+		// Add and implement current listener (called if current changes)
+		c12.addListener(new BrickletCurrent12.CurrentListener() {
+			public void current(short current) {
+				System.out.println("Current: " + current/1000.0 + " A");
+			}
+		});
+		
+		System.out.println("Press ctrl+c to exit");
+		ipcon.joinThread();
+	}
+}
