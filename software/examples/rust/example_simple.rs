aa07e6bb63df802d83b17bfee5e99b572d85084c
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:19:47 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,21 +1,21 @@
 use std::{error::Error, io};
 
-use tinkerforge::{current12_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{current12_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Current12 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Current12 Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let current12_bricklet = Current12Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let c = Current12Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current current
-    let current = current12_bricklet.get_current().recv()?;
-    println!("Current: {}{}", current as f32 / 1000.0, " A");
+    // Get current current.
+    let current = c.get_current().recv()?;
+    println!("Current: {} A", current as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
